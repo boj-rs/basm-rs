ec94c18274a03e0c2657ee0585ccd907e01046dc
kiwiyou <kiwiyou@kiwiyou.dev>
2022-06-02 18:38:35 +0900

    Resolve build.rs crash

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cargo build --release
-objcopy --strip-unneeded -j .init -j .text -j .rodata -O binary target/release/basm-rs basm.bin
+cargo +nightly build --release
+objcopy --strip-unneeded -j .init -j .text -j .rodata -O binary target/x86_64-unknown-linux-gnu/release/basm-rs basm.bin
 python3 gen.py
 
