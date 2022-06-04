1a39004fb5661001b533637f4aa3f2d9d7740bfa
Jeong-Min Lee <falsetru@gmail.com>
2022-06-04 23:34:48 +0900

    print(), println()

--- src/io_ext.rs
+++ src/io_ext.rs
@@ -1,4 +1,4 @@
-use crate::io::Reader;
+use crate::io::{Reader, Writer};
 
 impl<const N: usize> Reader<N> {
     pub fn iter_long(&mut self) -> LongIterator<N> {
@@ -45,5 +45,50 @@ impl<'a, const N: usize> Iterator for UintIterator<'a, N> {
 
     fn next(&mut self) -> Option<Self::Item> {
         Some(self.inner.next_uint())
+    }
+}
+
+pub trait Print<T> {
+    fn print(&mut self, x: T);
+    fn println(&mut self, x: T);
+}
+
+impl<const N: usize> Print<&[u8]> for Writer<N> {
+    fn print(&mut self, x: &[u8]) {
+        self.write(x);
+    }
+    fn println(&mut self, x: &[u8]) {
+        self.write(x);
+        self.write(b"\n");
+    }
+}
+
+impl<const N: usize> Print<i32> for Writer<N> {
+    fn print(&mut self, x: i32) {
+        self.write_int(x);
+    }
+    fn println(&mut self, x: i32) {
+        self.write_int(x);
+        self.write(b"\n");
+    }
+}
+
+impl<const N: usize> Print<i64> for Writer<N> {
+    fn print(&mut self, x: i64) {
+        self.write_long(x);
+    }
+    fn println(&mut self, x: i64) {
+        self.write_long(x);
+        self.write(b"\n");
+    }
+}
+
+impl<const N: usize> Print<usize> for Writer<N> {
+    fn print(&mut self, x: usize) {
+        self.write_uint(x);
+    }
+    fn println(&mut self, x: usize) {
+        self.write_uint(x);
+        self.write(b"\n");
     }
 }
