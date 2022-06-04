fe22cedfbb26ae430f4a8ae9794d61ec58753f41
kiwiyou <kiwiyou@kiwiyou.dev>
2022-06-04 13:12:39 +0900

    Use unsigned long long in gen.py

--- gen.py
+++ gen.py
@@ -1,5 +1,5 @@
 import os
-import struct
+import array
 
 text = "basm.bin"
 
@@ -7,5 +7,7 @@ code = bytearray((os.path.getsize(text) + 7) // 8 * 8)
 with open(text, "rb") as f:
     f.readinto(code)
 
-r = ",".join(min(str(i), hex(i), key=len) for i, in struct.iter_unpack("<q", code))
-print('__attribute__((section(".text")))long long a[]={%s};__libc_start_main(){((int(*)())a)();}main;' % r)
+arr = array.array('Q')
+arr.frombytes(code)
+r = ",".join(min(str(i), hex(i), key=len) for i in arr)
+print('__attribute__((section(".text")))unsigned long long a[]={%s};__libc_start_main(){((int(*)())a)();}main;' % r)
