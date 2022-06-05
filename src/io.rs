ab9a3d09782f7323fa5fd1c6b7ac8ef0f09bbdf6
kiwiyou <kiwiyou@kiwiyou.dev>
2022-06-05 17:30:30 +0900

    Run format

--- src/io.rs
+++ src/io.rs
@@ -13,11 +13,18 @@ impl<const N: usize> Writer<N> {
         while self.1 + buf.len() > N {
             let (current, next) = buf.split_at(N - self.1);
             buf = next;
-            self.0[self.1..].iter_mut().zip(current).for_each(|(d, &s)| { d.write(s); });
+            self.0[self.1..]
+                .iter_mut()
+                .zip(current)
+                .for_each(|(d, &s)| {
+                    d.write(s);
+                });
             self.1 = N;
             self.flush();
         }
-        self.0[self.1..].iter_mut().zip(buf).for_each(|(d, &s)| { d.write(s); });
+        self.0[self.1..].iter_mut().zip(buf).for_each(|(d, &s)| {
+            d.write(s);
+        });
         self.1 += buf.len();
     }
     #[inline(always)]
