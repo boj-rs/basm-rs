0d2395a6b02c7733fa5d936a4b64a728bcc414dd
kiwiyou <kiwiyou@kiwiyou.dev>
2022-06-16 02:41:22 +0900

    Fix long code RTE in rust template

--- scripts/template.rs
+++ scripts/template.rs
@@ -1,1 +1,1 @@
-#![crate_type="cdylib"]#![no_std]#[no_link]extern crate std;#[no_mangle]static _start:[u64;%(len)s]=[%(text)s];
+#![crate_type="cdylib"]#![no_std]#[no_link]extern crate std;#[link_section=".text"]#[no_mangle]static _start:[u64;%(len)s]=[%(text)s];
