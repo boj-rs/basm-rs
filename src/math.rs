5dddeb9eda004e80dee4ae818a1a31ec2697bad3
kiwiyou <kiwiyou@kiwiyou.dev>
2022-06-17 08:37:00 +0900

    Add more gcd and lcm

--- src/math.rs
+++ src/math.rs
@@ -2,31 +2,40 @@ pub mod miller_rabin;
 pub use miller_rabin::*;
 
 // reference: https://lemire.me/blog/2013/12/26/fastest-way-to-compute-the-greatest-common-divisor/
-pub fn gcd(mut a: u32, mut b: u32) -> u32 {
-    if a == 0 {
-        b
-    } else if b == 0 {
-        a
-    } else {
-        let shift = (a | b).trailing_zeros();
-        a >>= shift;
-        loop {
-            b >>= b.trailing_zeros();
-            b = b.wrapping_sub(a);
-            let m = ((b as i32) >> 31) as u32;
-            a = a.wrapping_add(b & m);
-            b = b.wrapping_add(m) ^ m;
-            if b == 0 {
-                break;
+
+macro_rules! define_gcd_lcm {
+    ($gcd:ident, $lcm:ident, $unsigned:ty, $signed:ty) => {
+        pub fn $gcd(mut a: $unsigned, mut b: $unsigned) -> $unsigned {
+            if a == 0 {
+                b
+            } else if b == 0 {
+                a
+            } else {
+                let shift = (a | b).trailing_zeros();
+                a >>= shift;
+                loop {
+                    b >>= b.trailing_zeros();
+                    b = b.wrapping_sub(a);
+                    let m = ((b as $signed) >> (<$signed>::BITS - 1)) as $unsigned;
+                    a = a.wrapping_add(b & m);
+                    b = b.wrapping_add(m) ^ m;
+                    if b == 0 {
+                        break;
+                    }
+                }
+                a << shift
             }
         }
-        a << shift
-    }
+
+        pub fn $lcm(a: $unsigned, b: $unsigned) -> $unsigned {
+            a / $gcd(a, b) * b
+        }
+    };
 }
 
-pub fn lcm(a: u32, b: u32) -> u32 {
-    a / gcd(a, b) * b
-}
+define_gcd_lcm!(gcd, lcm, u32, i32);
+define_gcd_lcm!(gcd_u64, lcm_u64, u64, i64);
+define_gcd_lcm!(gcd_usize, lcm_usize, usize, isize);
 
 #[cfg(test)]
 mod test {
@@ -40,5 +49,15 @@ mod test {
     #[test]
     fn lcm_returns_lcm() {
         assert_eq!(249318024, lcm(234984, 12732));
+    }
+
+    #[test]
+    fn gcd_u64_returns_gcd() {
+        assert_eq!(6, gcd_u64(2763162631554, 1276921782234));
+    }
+
+    #[test]
+    fn lcm_u64_returns_lcm() {
+        assert_eq!(4264971179382324, lcm_u64(273652348, 62341452));
     }
 }
