54e6218a66fa0b87b3e8e3b84565f6baf909bfb7
kiwiyou <kiwiyou@kiwiyou.dev>
2022-10-06 18:09:07 +0900

    Change rust alignment

--- scripts/template.rs
+++ scripts/template.rs
@@ -1,1 +1,1 @@
-#![crate_type="cdylib"]#![no_std]#[no_link]extern crate std;#[repr(align(256))]struct C([u64;%(len)s]);#[link_section=".text"]#[no_mangle]static _start:C=C([%(text)s]);
+#![crate_type="cdylib"]#![no_std]#[no_link]extern crate std;#[repr(align(32))]struct C([u64;%(len)s]);#[link_section=".text"]#[no_mangle]static _start:C=C([%(text)s]);
