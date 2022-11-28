c1528aa99bc4c1b92ce2d240d81260eb04b5e4d1
kiwiyou <kiwiyou@kiwiyou.dev>
2022-11-28 12:15:08 +0900

    Add featuring text

--- scripts/template.asm
+++ scripts/template.asm
@@ -1,2 +1,4 @@
+; Generated with https://github.com/kiwiyou/basm-rs
+; Learn rust and get high performance out of the box ☆ https://doc.rust-lang.org/book/
 global main
 main:dq %(text)s
--- scripts/template.c
+++ scripts/template.c
@@ -1,1 +1,3 @@
+// Generated with https://github.com/kiwiyou/basm-rs
+// Learn rust and get high performance out of the box ☆ https://doc.rust-lang.org/book/
 __attribute__((section(".text"),aligned(256)))unsigned long long __libc_start_main[]={%(text)s};main;
