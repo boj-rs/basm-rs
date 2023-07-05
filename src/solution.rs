8d0911f7a8f75a7068c9ff676fddd6187e076cc5
Byeongkeun Ahn <7p54ks3@naver.com>
2023-07-05 17:13:09 +0900

    Revert solution.rs to the original one

--- src/solution.rs
+++ src/solution.rs
@@ -1,17 +1,10 @@
-use basm::io;
+use basm::io::{Reader, Writer};
 
 #[inline(always)]
 pub fn main() {
-    const LIST: [&[u8]; 2] = [b"ki", b"wi"];
-    let buf = [0u8; 1];
-    let n = buf.as_ptr() as usize;
-    let mut writer = io::Writer::<{ 1 << 15 }>::new();
-    let mut i = n % 2;
-    let mut j = 1 - i;
-    if i > j {
-        (i, j) = (j, i);
-    }
-    writer.write(LIST[i]);
-    writer.write(LIST[j]);
-    writer.write(b"\n");
+    let mut reader: Reader = Default::default();
+    let mut writer: Writer = Default::default();
+    let a = reader.next_usize();
+    let b = reader.next_usize();
+    writer.write_usize(a + b);
 }
