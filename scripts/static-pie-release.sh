9d367f41772083bde29044698c9b02836ee8666c
Byeongkeun Ahn <7p54ks3@naver.com>
2023-07-05 17:53:13 +0900

    Strip away debug infos before generating C code

--- scripts/static-pie-release.sh
+++ scripts/static-pie-release.sh
@@ -3,4 +3,6 @@ set -e
 
 binary_path=basm.bin
 cargo +nightly build --release "$@"
-python3 scripts/static-pie-gen.py target/x86_64-unknown-linux-gnu/release/basm scripts/static-pie-template.c
+cp target/x86_64-unknown-linux-gnu/release/basm target/x86_64-unknown-linux-gnu/release/basm_stripped
+objcopy --strip-all target/x86_64-unknown-linux-gnu/release/basm_stripped
+python3 scripts/static-pie-gen.py target/x86_64-unknown-linux-gnu/release/basm_stripped scripts/static-pie-template.c
