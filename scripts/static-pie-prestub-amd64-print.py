d4bc8e4466f30d7d04fb761f7077bb299eb4d657
Byeongkeun Ahn <7p54ks3@naver.com>
2023-10-20 16:58:28 +0900

    amd64-rust: shorten the prestub embedding

--- scripts/static-pie-prestub-amd64-print.py
+++ scripts/static-pie-prestub-amd64-print.py
@@ -10,9 +10,19 @@ while len(prestub) % 8 != 0:
 # convert each 8-byte chunk
 out = []
 for i in range(0, len(prestub), 8):
-    qword = str(hex(int.from_bytes(prestub[i:i+8], "little")))
-    line = "        \".quad {0}\",\n".format(qword)
-    out.append(line)
+    if i % 32 == 0:
+        out.append("        \".quad ")
+    x = int.from_bytes(prestub[i:i+8], "little")
+    qword1 = str(hex(x))
+    qword2 = str(x)
+    if len(qword1) <= len(qword2):
+        out.append(qword1)
+    else:
+        out.append(qword2)
+    if i % 32 == 24 or i + 8 == len(prestub):
+        out.append("\",\n")
+    else:
+        out.append(",")
 
 # print the result
 print("".join(out))
