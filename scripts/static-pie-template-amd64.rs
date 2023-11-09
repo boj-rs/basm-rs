50d6f07a23080bf97573b3c0139b8a7fa5ef6c43
Byeongkeun Ahn <7p54ks3@naver.com>
2023-11-09 22:03:50 +0900

    Update static-pie-template-amd64.rs

--- scripts/static-pie-template-amd64.rs
+++ scripts/static-pie-template-amd64.rs
@@ -2,7 +2,7 @@
 // Learn rust and get high performance out of the box! See: https://doc.rust-lang.org/book/
 
 #![crate_type = "cdylib"] // To compile on Windows, remove this line or pass '--crate-type=bin' to rustc to avoid creating a DLL.
-#![cfg_attr(not(windows), no_std)]#[no_link]extern crate std as _;
+#![cfg_attr(not(windows), no_std)]#![allow(unused)]#[no_link]extern crate std as _;
 
 // SOLUTION BEGIN
 #[cfg(any())] mod solution {
@@ -25,7 +25,7 @@ static mut PAYLOAD: [u8; $$$$binary_base85_len$$$$] = *b$$$$binary_base85$$$$;
 
 #[no_mangle]
 unsafe fn _start() {
-    let _p = (0, 0); #[cfg(windows)] let _p = (LoadLibraryA(b"kernel32\0".as_ptr()), GetProcAddress);
+    let p = (0, 0); #[cfg(windows)] let p = (LoadLibraryA(b"kernel32\0".as_ptr()), GetProcAddress);
     core::arch::asm!(
         ".quad 50505157f0e48348h,940fff8548c03150h,5450c0ff50c589c0h,881d8d4880c48348h,\
         480c75ed85h<<24,0ff2f538d4822c383h,0ff10b5c9315f50d7h,480f75ed859748d3h,\
@@ -35,8 +35,8 @@ unsafe fn _start() {
         0c80fea7c05fd83c5h,0ff3158096ae1ebabh,41226a5a076ace89h,0c931455841ff6a5ah,\
         0b848c3050fh,315a51h<<40,6a00003000b841c9h,0ff28ec8348594140h,\
         0c328c48348d0h\n.asciz \"09AZaz!!#&(+--;@^`{{~VirtualAlloc\"",
-        in("rcx") _p.0, in("rdi") _p.1, in("r14") PAYLOAD.as_mut_ptr(), in("r13") b$$$$stub_base85$$$$.as_ptr()
+        in("rcx") p.0, in("rdi") p.1, in("r14") PAYLOAD.as_mut_ptr(), in("r13") $$$$stub_base85$$$$.as_ptr()
     )
 }
-#[allow(dead_code)] fn main() { unsafe { _start() } }
+fn main() { unsafe { _start() } }
 // LOADER END
