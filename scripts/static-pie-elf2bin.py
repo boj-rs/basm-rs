254a32a02548c8c872bb4b94a391ee27c0a3723b
Byeongkeun Ahn <7p54ks3@naver.com>
2023-11-19 21:56:30 +0900

    Optimize non-native patch for size

--- scripts/static-pie-elf2bin.py
+++ scripts/static-pie-elf2bin.py
@@ -234,12 +234,12 @@ if __name__ == '__main__':
 
     # Patch the entrypoint
     # We look for:
-    #   0:  6a 00                   push   0x0
+    #   0:  f8                      clc
     # and replace it with:
-    #   0:  6a 01                   push   0x1
+    #   0:  f9                      stc
     # This works for both i686 and amd64.
-    assert memory_bin[entrypoint_offset:entrypoint_offset+2] == b"\x6a\x00"
-    memory_bin[entrypoint_offset:entrypoint_offset+2] = b"\x6a\x01"
+    assert memory_bin[entrypoint_offset:entrypoint_offset+1] == b"\xf8"
+    memory_bin[entrypoint_offset:entrypoint_offset+1] = b"\xf9"
 
     with open(binary_path, "wb") as f:
         f.write(bytes(memory_bin))
