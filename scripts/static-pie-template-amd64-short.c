31124d44498dc7cdef72e3afedafa10ce7052fb3
Byeongkeun Ahn <7p54ks3@naver.com>
2023-11-24 22:06:04 +0900

    short: update static-pie-template-amd64-short.c

--- scripts/static-pie-template-amd64-short.c
+++ scripts/static-pie-template-amd64-short.c
@@ -34,14 +34,7 @@ typedef struct {
     void *fn_table[6];
 } PLATFORM_DATA;
 #pragma pack(pop)
-BASMCALL void *svc_alloc_rwx(size_t size) {
-    return (void *) syscall(9, NULL, size, 0x7, 0x22, -1, 0);
-}
 typedef int (BASMCALL *stub_ptr)(void *, void *);
-__attribute__ ((section (".text#"))) const char stub_raw[] = $$$$stub_raw$$$$;
-stub_ptr get_stub() {
-    return (stub_ptr) stub_raw;
-}
 char payload[][$$$$min_len_4096$$$$] = $$$$binary_base85$$$$;
 int main() {}
 #ifdef __cplusplus
@@ -58,16 +51,14 @@ int __libc_start_main(
     PLATFORM_DATA pd;
     pd.env_id = 2;
     pd.env_flags = 1;
-    u8 stubbuf[68 + $$$$stub_len$$$$] = "QMd~L002n8@6D@;XGJ3cz5oya01pLO>naZmS5~+Q0000n|450>x(5IN07=KfA^-pYO)<bp|Hw@-$qxlyU&9Xz]";
-    b85tobin(stubbuf, (char const *)stubbuf);
-    for (size_t i = 0; i < $$$$stub_len$$$$; i++) stubbuf[68 + i] = (u8)stub_raw[i];
-    size_t base = ((size_t)stub_raw) & 0xFFFFFFFFFFFFF000ULL;
-    size_t len = (((size_t)stub_raw) + sizeof(stub_raw)) - base;
-    len = ((len + 0xFFF) >> 12) << 12;
+    u8 stubbuf[68 + $$$$stub_len$$$$];
+    b85tobin(stubbuf, "QMd~L002n8@6D@;XGJ3cz5oya01pLO>naZmS5~+Q0000n|450>x(5IN07=KfA^-pYO)<bp|Hw@-$qxlyU&9Xz]");
+    b85tobin(stubbuf + 68, $$$$stub_base85$$$$);
+    size_t base = ((size_t)main) & 0xFFFFFFFFFFFFF000ULL;
     *(u64 *)(stubbuf + 0x08) = (u64) base;
-    *(u32 *)(stubbuf + 0x11) = (u32) len;
+    *(u32 *)(stubbuf + 0x11) = (u32) 4096;
     base = ((size_t)stubbuf) & 0xFFFFFFFFFFFFF000ULL;
-    len = (((size_t)stubbuf) + 68 + $$$$stub_len$$$$) - base;
+    size_t len = (((size_t)stubbuf) + 68 + $$$$stub_len$$$$) - base;
     len = ((len + 0xFFF) >> 12) << 12;
     syscall(10, base, len, 0x7);
     pd.fn_table[0] = (void *) (stubbuf + 0x1c);
