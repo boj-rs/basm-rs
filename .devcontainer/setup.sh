e6bd2713592f4a8229020d8dd77fb8b09382f8eb
Byeongkeun Ahn <7p54ks3@naver.com>
2023-11-29 13:56:48 +0900

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+apt-get -y update
+apt-get -y install curl git gnupg2 jq sudo vim build-essential openssl python3
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain nightly --default-host x86_64-unknown-linux-gnu -y
 sudo ln -s /home/codespace/.cargo/bin/cargo /bin/cargo
-code --install-extension rust-lang.rust-analyzer
-code --install-extension vadimcn.vscode-lldb
