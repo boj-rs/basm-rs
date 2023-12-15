6038868b2aa5c2034201c95344a366dfe64de929
Byeongkeun Ahn <7p54ks3@naver.com>
2023-12-15 14:50:22 +0900

    fn-impl: validate arg/ret types + encode arg names

--- basm-macro/src/lib.rs
+++ basm-macro/src/lib.rs
@@ -9,10 +9,24 @@ use quote::{quote, ToTokens};
 use syn::{parse_macro_input, ItemFn};
 
 use data_encoding::HEXLOWER;
+use std::collections::HashSet;
 use std::fmt::Write;
 
 fn mangle(input: &str) -> String {
     String::from("_basm_export_") + &HEXLOWER.encode(input.as_bytes())
+}
+
+fn check_type(input: &str) -> bool {
+    let base_types = ["i8", "i16", "i32", "i64", "isize", "u8", "u16", "u32", "u64", "usize"];
+    let mut derived_types = HashSet::new();
+    for &ty in base_types.iter() {
+        derived_types.insert(ty.to_string());
+        let x = format!("* const {0}", &ty).to_string();
+        derived_types.insert(x);
+        let x = format!("* mut {0}", &ty).to_string();
+        derived_types.insert(x);
+    }
+    derived_types.contains(input)
 }
 
 #[proc_macro_attribute]
@@ -31,7 +45,6 @@ pub fn basm_export(_attr: TokenStream, item: TokenStream) -> TokenStream {
 
     let inputs = &fn_in.sig.inputs;
     let mut input_names = vec![];
-    let mut input_types = vec![];
     for tok in inputs.iter() {
         match tok.clone() {
             syn::FnArg::Receiver(_) => { panic!(); }
@@ -42,7 +55,10 @@ pub fn basm_export(_attr: TokenStream, item: TokenStream) -> TokenStream {
                     panic!();
                 }
                 let a_ty = &a.ty;
-                input_types.push(quote!{#a_ty}.to_string());
+                let a_ty = quote!{#a_ty}.to_string();
+                if !check_type(&a_ty) {
+                    panic!("Unsupported input type \"{}\"", &a_ty);
+                }
             }
         }
     }
@@ -50,7 +66,11 @@ pub fn basm_export(_attr: TokenStream, item: TokenStream) -> TokenStream {
         syn::punctuated::Punctuated::from_iter(input_names);
     let output = &fn_in.sig.output;
     let output_type = if let syn::ReturnType::Type(_, x) = output {
-        "_to_".to_owned() + &quote!{#x}.to_string()
+        let o_ty = quote!{#x}.to_string();
+        if !check_type(&o_ty) {
+            panic!("Unsupported output type \"{}\"", &o_ty);
+        }
+        "_to_".to_owned() + &o_ty
     } else {
         String::new()
     };
@@ -58,7 +78,7 @@ pub fn basm_export(_attr: TokenStream, item: TokenStream) -> TokenStream {
     /* name mangling */
     let fn_name = &fn_in.sig.ident;
     let mut fn_name_out = String::new();
-    write!(&mut fn_name_out, "{0}_{1}{2}", &fn_name, input_types.join("_"), &output_type).unwrap();
+    write!(&mut fn_name_out, "{0}_{1}{2}", &fn_name, &quote!{#inputs}.to_string(), &output_type).unwrap();
     let fn_name_out = mangle(&fn_name_out);
     let fn_name_out: TokenStream2 = fn_name_out.parse().unwrap();
 
