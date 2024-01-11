d20fe03d1e620e1722ed1cb7a83f1f1c61628ed3
Byeongkeun Ahn <7p54ks3@naver.com>
2024-01-11 22:24:59 +0900

    fn-impl: update import.rs

--- basm-macro/src/import.rs
+++ basm-macro/src/import.rs
@@ -3,6 +3,7 @@ extern crate quote;
 extern crate syn;
 
 use proc_macro2::TokenStream;
+use quote::quote;
 use syn::{Signature, parse::{Parse, ParseStream}, Result, Token};
 
 struct VecSignature {
@@ -21,20 +22,27 @@ impl Parse for VecSignature {
     }
 }
 
-pub fn import_impl(input: TokenStream) -> TokenStream {
-    let vecsig: VecSignature = syn::parse2(input).unwrap();
-    for sig in vecsig.sigs {
-        super::utils::verify_signature(&sig);
-        for tok in sig.inputs {
-            match tok {
-                syn::FnArg::Receiver(_) => {
-                    // self, &self, &mut self are not allowed
-                    panic!();
-                }
-                syn::FnArg::Typed(_pattype) => {
-                }
+fn import_impl_single(sig: Signature) -> TokenStream {
+    super::utils::verify_signature(&sig);
+    for tok in sig.inputs {
+        match tok {
+            syn::FnArg::Receiver(_) => {
+                // self, &self, &mut self are not allowed
+                panic!();
+            }
+            syn::FnArg::Typed(_pattype) => {
             }
         }
     }
     "".parse().unwrap()
+}
+
+pub fn import_impl(input: TokenStream) -> TokenStream {
+    let vecsig: VecSignature = syn::parse2(input).unwrap();
+    let out: Vec<_> = vecsig.sigs.into_iter().map(|sig| {
+        import_impl_single(sig)
+    }).collect();
+    quote! {
+        #(#out)*
+    }
 }
