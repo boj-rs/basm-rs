9fdb4ad70d89be674cf6f0f501dbac1be2344824
Byeongkeun Ahn <7p54ks3@naver.com>
2024-01-11 23:41:50 +0900

    fn-impl: add eat

--- basm-std/src/serialization/mod.rs
+++ basm-std/src/serialization/mod.rs
@@ -11,3 +11,13 @@ mod serialize;
 pub use serialize::Ser as Serialize;
 mod deserialize;
 pub use deserialize::De as Deserialize;
+
+pub unsafe fn eat(ptr_serialized: usize) -> (&'static [u8], usize) {
+    const SIZE: usize = core::mem::size_of::<usize>();
+    let mut buf = core::slice::from_raw_parts(ptr_serialized as *const u8, SIZE);
+    let len = usize::de(&mut buf);
+    let remain = core::slice::from_raw_parts((ptr_serialized + SIZE) as *const u8, len + SIZE);
+    let mut buf = &remain[len..];
+    let ptr_fn = usize::de(&mut buf);
+    (&remain[..len], ptr_fn)
+}
