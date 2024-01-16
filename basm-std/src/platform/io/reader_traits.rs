112e5640d6c883a4877504b16e369ff5adbbe8b1
Byeongkeun Ahn <7p54ks3@naver.com>
2024-01-16 23:48:37 +0900

    Implement Readable for arrays and tuples

--- basm-std/src/platform/io/reader_traits.rs
+++ basm-std/src/platform/io/reader_traits.rs
@@ -29,3 +29,34 @@ impl Readable for Line {
         Self(reader.line())
     }
 }
+
+impl<T: Readable, const N: usize> Readable for [T; N] {
+    fn read(reader: &mut impl ReaderTrait) -> Self {
+        core::array::from_fn(|_| T::read(reader))
+    }
+}
+
+impl Readable for () {
+    fn read(_reader: &mut impl ReaderTrait) -> Self {
+    }
+}
+
+impl<T: Readable> Readable for (T,) {
+    fn read(reader: &mut impl ReaderTrait) -> Self {
+        (T::read(reader),)
+    }
+}
+
+macro_rules! impl_tuple {
+    ($u:ident) => {};
+    ($u:ident $($t:ident)+) => {
+        impl<$u: Readable, $($t: Readable),+> Readable for ($u, $($t),+) {
+            fn read(reader: &mut impl ReaderTrait) -> Self {
+                ($u::read(reader), $($t::read(reader)),+)
+            }
+        }
+        impl_tuple!($($t) +);
+    };
+}
+
+impl_tuple!(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z);
