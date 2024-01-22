11ab906229e76d12dc135e03df05fda6177d5787
Byeongkeun Ahn <7p54ks3@naver.com>
2024-01-22 12:51:44 +0900

    CI: test `cargo test` for crate `basm`

--- basm/src/bin/basm-lib.rs
+++ basm/src/bin/basm-lib.rs
@@ -14,3 +14,15 @@ extern crate basm_std as basm;
 #[allow(dead_code)]
 #[path = "../solution.rs"]
 mod solution;
+
+#[cfg(test)]
+mod verify_test_works {
+    fn add(x: i64, y: i64) -> i64 {
+        x + y
+    }
+
+    #[test]
+    fn run() {
+        assert_eq!(8, add(5, 3));
+    }
+}
