5e622e26701cc3e179d2636d83f2eaf746cedf4a
Byeongkeun Ahn <7p54ks3@naver.com>
2024-01-24 13:04:33 +0900

    scripts: pack all .rs files except src/bin/*

--- scripts/srcpack.py
+++ scripts/srcpack.py
@@ -3,13 +3,30 @@ import os
 # Reads and assembles the source code in the crate at the path `crate_root`.
 # `crate_root` usually equals `basm/`.
 def read_assemble(crate_root, target_language):
-    solution_src_path = os.path.join(crate_root, "src/solution.rs")
-    with open(solution_src_path, encoding='utf8') as f:
-        sol = f.readlines()
-    if target_language in ["Rust", "HTML"]:
-        return assemble_as_is(sol)
+    sol_all = []
+    crate_src_path = os.path.join(crate_root, "src/")
+    for root, dirs, files in os.walk(crate_src_path):
+        if os.path.abspath(root).startswith(os.path.abspath(os.path.join(crate_src_path, "bin/"))):
+            continue
+        for f in files:
+            f_path = os.path.join(root, f)
+            if f_path.endswith(".rs"):
+                with open(f_path, encoding='utf8') as f:
+                    sol = f.readlines()
+                sol_all.append((f_path, sol))
+    if len(sol_all) == 1:
+        sol_flat = sol_all[0][1]
     else:
-        return assemble_with_commenting(sol)
+        sol_flat = []
+        for i, (f_path, sol) in enumerate(sol_all):
+            if i > 0:
+                sol_flat.append("\n")
+            sol_flat.append("// {0}\n".format(f_path))
+            sol_flat.extend(sol)
+    if target_language in ["Rust", "HTML"]:
+        return assemble_as_is(sol_flat)
+    else:
+        return assemble_with_commenting(sol_flat)
 
 def assemble_as_is(sol):
     sol = [line.replace("\ufeff", "") for line in sol]
