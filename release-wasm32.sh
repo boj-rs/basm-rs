c6545bcf8c364cbf066d9c46c29ff4557500eac0
Byeongkeun Ahn <7p54ks3@naver.com>
2024-02-03 23:28:41 +0900

    aarch64: fix CI again

--- release-wasm32.sh
+++ release-wasm32.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 >&2 echo "Building project for target wasm32-unknown-unknown, language JavaScript, build mode Release"
 cargo +nightly build --target wasm32-unknown-unknown --bin=basm-submit --features=submit --release "$@"
 python3 scripts/wasm-gen.py wasm-template.js JavaScript
