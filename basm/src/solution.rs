10e244d1997e51d68a016748f379691df19b4ff4
BamgoeSN <60645387+BamgoeSN@users.noreply.github.com>
2024-02-06 02:10:07 +0900

    Restore solution.rs

--- basm/src/solution.rs
+++ basm/src/solution.rs
@@ -1,11 +1,8 @@
-use basm::platform::io::{Print, Reader, ReaderTrait, Writer};
-
-use basm::utils::*;
+use basm::platform::io::{Reader, ReaderTrait, Writer, Print};
 pub fn main() {
     let mut reader: Reader = Default::default();
     let mut writer: Writer = Default::default();
-    let a = reader.f64();
-    let b = reader.f64();
-    writer.println(a.atan2(b));
+    let a = reader.i64();
+    let b = reader.i64();
+    writer.println(a + b);
 }
-
