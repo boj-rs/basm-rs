d9ade91a6e759801320d5df059dc56d2ab176384
Byeongkeun Ahn <7p54ks3@naver.com>
2024-02-19 17:38:21 +0900

    math: use faster factorization routine by wider93

--- basm-std/src/math/pollard_rho.rs
+++ basm-std/src/math/pollard_rho.rs
@@ -1,67 +1,100 @@
-use alloc::vec;
-use alloc::vec::Vec;
-use super::miller_rabin::is_prime_u64;
-use super::gcd;
+// This is a Rust port of the implementation originally written in C++ by wider93.
 
-use rand::{RngCore, SeedableRng};
-use rand::rngs::SmallRng;
+use alloc::{vec, vec::Vec};
+use crate::math::{gcd, is_prime_u64, miller_rabin::{M, OddMont}};
 
-fn rho(n: u64, small_rng: &mut SmallRng) -> u64 {
-    if n % 2 == 0 { return 2; }
-
-    let x0 = 2;
-    let c = small_rng.next_u64() % n;
-    let f = |x: u64| -> u64 {
-        let x2 = ((x as u128 * x as u128) % n as u128) as u64;
-        (x2 + c) % n
-    };
-
-    let (mut p, mut q, mut g) = (x0, x0, 1);
-    while g == 1 {
-        p = f(p);
-        q = f(f(q));
-        if p == q {
-            g = n;
-        } else {
-            g = gcd(p.abs_diff(q), n);
-        }
-    }
-    g
+trait PollardRhoOp<T> {
+    fn pollard_rho_type(r: T, init: T) -> T;
 }
 
-pub fn factorize(mut x: u64) -> Vec<u64> {
-    assert!(x > 0);
-    let mut factors = vec![];
-    for p in [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37] {
-        while x % p == 0 {
-            factors.push(p);
-            x /= p;
-        }
-    }
-    let mut q = vec![];
-    if x > 1 {
-        q.push(x);
-    }
-
-    let random_thing = [0; 1];
-    let mut small_rng = SmallRng::seed_from_u64(random_thing.as_ptr() as usize as u64);
-    while let Some(n) = q.pop() {
-        if is_prime_u64(n) {
-            factors.push(n);
-        } else {
-            let mut f;
-            loop {
-                f = rho(n, &mut small_rng);
-                if f != 0 && f != 1 && f != n {
-                    break;
+macro_rules! impl_pollard_rho_type {
+    ($ty: ident, $ty_large: ident) => {
+        impl PollardRhoOp<$ty> for $ty {
+            fn pollard_rho_type(r: $ty, init: $ty) -> $ty {
+                let rm = OddMont::<$ty>::new(r);
+                let mut x0 = 2;
+                let mut k = r;
+                const STEP: u32 = 1 << 10;
+                while k == r {
+                    let mut y = M::<$ty> { v: x0 };
+                    x0 += 1;
+                    k = 1;
+                    let mut itr = STEP;
+                    while k == 1 && itr < (1 << 21) {
+                        let mut g = M::<$ty> { v: 1 };
+                        let x = y;
+                        for _ in (0..itr).step_by(STEP as usize) {
+                            if k != 1 { break; }
+                            for _ in 0..STEP {
+                                y = rm.redc(y.v as $ty_large * y.v as $ty_large + init as $ty_large);
+                                g = rm.mul(g, rm.to_mont(x.v.abs_diff(y.v)));
+                            }
+                            k = gcd(g.v, r);
+                            if k == r {
+                                k = 1;
+                                let mut py = x;
+                                for _ in 0..STEP {
+                                    py = rm.redc(py.v as $ty_large * py.v as $ty_large + init as $ty_large);
+                                    k = gcd(r, x.v.abs_diff(py.v));
+                                    if k != 1 { break; }
+                                }
+                                if k == 1 {
+                                    k = r;
+                                }
+                            }
+                        }
+                        itr <<= 1;
+                    }
                 }
+                k
             }
-            q.push(f);
-            q.push(n / f);
         }
     }
-    factors.sort_unstable();
-    factors
+}
+impl_pollard_rho_type!(u32, u64);
+impl_pollard_rho_type!(u64, u128);
+
+fn pollard_rho(r: u64) -> u64 {
+    if r < (1u64 << 32) {
+        u32::pollard_rho_type(r as u32, 1) as u64
+    } else {
+        u64::pollard_rho_type(r, 1)
+    }
+}
+
+pub fn factorize(mut n: u64) -> Vec<u64> {
+    let mut v = Vec::new();
+    for p in [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37] {
+        while n % p == 0 {
+            v.push(p);
+            n /= p;
+        }
+    }
+    if n == 1 {
+        return v;
+    }
+    let mut q = vec![(n, 1)];
+    while let Some((t, num)) = q.pop() {
+        if t < 1681 || is_prime_u64(t) {
+            for _ in 0..num {
+                v.push(t);
+            }
+        } else {
+            let s = (libm::sqrt(t as f64) + 0.5) as u64;
+            if s * s == t {
+                q.push((s, num << 1));
+                continue;
+            }
+            let a = pollard_rho(t);
+            debug_assert!(a > 1);
+            debug_assert!(a < t);
+            debug_assert!(t % a == 0);
+            q.push((a, num));
+            q.push((t / a, num));
+        }
+    }
+    v.sort_unstable();
+    v
 }
 
 mod test {
