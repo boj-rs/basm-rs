cd56e05a21e5f62bc500982a6774522686ee855c
Byeongkeun Ahn <7p54ks3@naver.com>
2024-05-07 20:39:20 +0900

    Maxflow: add global relabeling

--- basm-std/src/graph/maxflow.rs
+++ basm-std/src/graph/maxflow.rs
@@ -13,7 +13,8 @@ struct Edge {
 /// Maximum flow solver without costs.
 pub struct FlowGraph {
     adj: Vec<Vec<usize>>,
-    e: Vec<Edge>
+    e: Vec<Edge>,
+    edge_count: usize
 }
 
 impl Default for FlowGraph {
@@ -24,7 +25,7 @@ impl Default for FlowGraph {
 
 impl FlowGraph {
     pub fn new() -> Self {
-        Self { adj: vec![], e: vec![] }
+        Self { adj: vec![], e: vec![], edge_count: 0 }
     }
     pub fn add_edge(&mut self, u: usize, v: usize, c: i64, bidirectional: bool) {
         assert!(c >= 0);
@@ -34,6 +35,7 @@ impl FlowGraph {
         self.e.push(Edge { v, c, f: 0 });
         self.adj[v].push(self.e.len());
         self.e.push(Edge { v: u, c: if bidirectional { c } else { 0 }, f: 0 });
+        self.edge_count += 1;
     }
     /// Solves the maximum flow problem with source `s` and sink `t`.
     /// `s` and `t` must be distinct.
@@ -47,6 +49,7 @@ impl FlowGraph {
             adj.resize(bound + 1, vec![]);
         }
         let n = adj.len();
+        let m = self.edge_count;
 
         let mut h = vec![0usize; n];
         h[s] = n;
@@ -73,8 +76,10 @@ impl FlowGraph {
             }
         }
 
+        // Phase I: find maximum flow to sink
+        let mut relabel_count = 0;
         let mut e_last = vec![0usize; n];
-        while head != usize::MAX {
+        'outermost: while head != usize::MAX {
             if let Some(u) = active[head].pop() {
                 debug_assert!(p[u] > 0);
                 'outer: loop {
@@ -110,15 +115,97 @@ impl FlowGraph {
                         }
                     }
                     (head, next[h[u]]) = (h[u], if active[head].is_empty() { next[head] } else { head });
+                    relabel_count += 1;
                 }
             } else {
                 // Panic for debugging
                 panic!();
             }
+            if relabel_count >= m {
+                h.fill(n);
+                h[t] = 0;
+                for active_u in active.iter_mut() {
+                    active_u.clear();
+                }
+
+                // Perform global relabeling.
+                // This takes O(m), hence if we perform global relabeling every m relabels
+                //   the amortized cost of global relabeling is O(1).
+                let mut visited = vec![false; n];
+                let mut queue = VecDeque::<usize>::new();
+                visited[t] = true;
+                queue.push_back(t);
+                while !queue.is_empty() {
+                    let u = queue.pop_front().unwrap();
+                    for &eid in &adj[u] {
+                        let v = e[eid].v;
+                        if e[eid ^ 1].c - e[eid ^ 1].f > 0 && !visited[v] && v != s && v != t {
+                            visited[v] = true;
+                            h[v] = h[u] + 1;
+                            queue.push_back(v);
+                        }
+                    }
+                }
+
+                for u in 0..n {
+                    if u != s && u != t && p[u] > 0 {
+                        active[h[u]].push(u);
+                    }
+                }
+                head = usize::MAX;
+                let mut lowest = usize::MAX;
+                for h in 0..=2*n {
+                    if !active[h].is_empty() {
+                        next[h] = head;
+                        head = h;
+                        lowest = min(lowest, h);
+                    }
+                }
+                if lowest == usize::MAX || lowest >= n {
+                    break 'outermost;
+                }
+                relabel_count = 0;
+            }
         }
 
+        // Phase II: send excesses back to source via doing a DFS from the source
+        fn dfs_phase2(adj: &Vec<Vec<usize>>, e: &mut [Edge], p: &mut [i64], h: &[usize], visited: &mut [bool], stack: &mut Vec<usize>, u: usize) {
+            if visited[u] {
+                // Back-edge; cancel the minimal flow along the cycle
+                let mut df = i64::MAX;
+                for &eid in stack.iter().rev() {
+                    df = min(df, e[eid].f);
+                    if e[eid ^ 1].v == u { break; }
+                }
+                for &eid in stack.iter().rev() {
+                    e[eid].f -= df;
+                    e[eid ^ 1].f += df;
+                    if e[eid ^ 1].v == u { break; }
+                }
+            } else {
+                // Continue on DFS
+                visited[u] = true;
+                for &eid in &adj[u] {
+                    let v = e[eid].v;
+                    if e[eid].f > 0 && h[v] >= adj.len() {
+                        stack.push(eid);
+                        dfs_phase2(adj, e, p, h, visited, stack, v);
+                        stack.pop();
+                        let df = e[eid].f;
+                        e[eid].f -= df;
+                        e[eid ^ 1].f += df;
+                        p[u] += df;
+                        p[v] -= df;
+                    }
+                }
+            }
+        }
+        let mut visited = vec![false; n];
+        let mut stack = vec![];
+        dfs_phase2(&adj, &mut e, &mut p, &h, &mut visited, &mut stack, s);
+
         // Compute cut
-        let mut visited = vec![false; adj.len()];
+        let mut visited = vec![false; n];
         let mut queue = VecDeque::<usize>::new();
         visited[s] = true;
         queue.push_back(s);
