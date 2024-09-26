309c31eb9fde8c9da4a69978694868cb26049cc9
bamgoe <60645387+BamgoeSN@users.noreply.github.com>
2024-09-26 13:52:41 +0900

    Remove TODOs from static_modint

--- basm-std/src/math/static_modint.rs
+++ basm-std/src/math/static_modint.rs
@@ -3,8 +3,6 @@
 
 use core::{fmt::Display, ops::*};
 
-use super::GcdOps;
-
 #[derive(Clone, Copy, PartialEq, Eq, Default, Debug)]
 pub struct ModInt<const M: u64>(u64);
 
@@ -32,7 +30,6 @@ impl<const M: u64> From<ModInt<M>> for u64 {
     }
 }
 
-// TODO: Handle the case for `self.0 + rhs.0` overflow (the implementation below is not enough)
 impl<const M: u64> Add for ModInt<M> {
     type Output = Self;
     fn add(self, rhs: Self) -> Self::Output {
@@ -45,7 +42,6 @@ impl<const M: u64> Add for ModInt<M> {
     }
 }
 
-// TODO: Same with `Add`
 impl<const M: u64> AddAssign for ModInt<M> {
     fn add_assign(&mut self, rhs: Self) {
         let (val, carry) = self.0.overflowing_add(rhs.0);
@@ -57,7 +53,6 @@ impl<const M: u64> AddAssign for ModInt<M> {
     }
 }
 
-// TODO: Try removing `i128` cast
 impl<const M: u64> Div for ModInt<M> {
     type Output = Self;
     /// May panic if M == 0
@@ -68,7 +63,6 @@ impl<const M: u64> Div for ModInt<M> {
     }
 }
 
-// TODO: Same with `Div`
 impl<const M: u64> DivAssign for ModInt<M> {
     fn div_assign(&mut self, rhs: Self) {
         let inv = super::egcd(rhs.0 as i128, M as i128).1;
@@ -77,7 +71,6 @@ impl<const M: u64> DivAssign for ModInt<M> {
     }
 }
 
-// TODO: Handle the case for `self.0 * rhs.0` overflow
 impl<const M: u64> Mul for ModInt<M> {
     type Output = Self;
     fn mul(self, rhs: Self) -> Self::Output {
@@ -85,14 +78,12 @@ impl<const M: u64> Mul for ModInt<M> {
     }
 }
 
-// TODO: Same with `Mul`
 impl<const M: u64> MulAssign for ModInt<M> {
     fn mul_assign(&mut self, rhs: Self) {
         self.0 = ((self.0 as u128 * rhs.0 as u128) % (M as u128)) as u64;
     }
 }
 
-// TODO: Check if there's a more performant way, doable without literal rem operation
 impl<const M: u64> Neg for ModInt<M> {
     type Output = Self;
     fn neg(self) -> Self::Output {
@@ -100,7 +91,6 @@ impl<const M: u64> Neg for ModInt<M> {
     }
 }
 
-// TODO: Is there any better way in terms of performance?
 impl<const M: u64> Sub for ModInt<M> {
     type Output = Self;
     fn sub(self, rhs: Self) -> Self::Output {
@@ -108,7 +98,6 @@ impl<const M: u64> Sub for ModInt<M> {
     }
 }
 
-// TODO: Same with `Sub`
 impl<const M: u64> SubAssign for ModInt<M> {
     fn sub_assign(&mut self, rhs: Self) {
         *self += -rhs;
