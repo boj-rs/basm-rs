f202f38c8f99953b79d34c5757356d640549353d
Byeongkeun Ahn <7p54ks3@naver.com>
2025-06-14 21:49:44 +0900

    feat: shorten the shorter-c template; __cplusplus
    Per C and C++ standards, any macro that is not defined will evaluate to zero. Any C compiler is obliged to not define __cplusplus.

--- scripts/templates/static-pie-template-amd64-shorter.c
+++ scripts/templates/static-pie-template-amd64-shorter.c
@@ -4,7 +4,7 @@
 $$$$solution_src$$$$
 // SOLUTION END
 __attribute__((section(".text#")))long long s[]={0xab880e4834890,0x5700001000be0000,0x5a076a08247c8d48,0xffffff000e78148,82502830085,0x65b95708247c8d48,-0x5b0cf73e7d<<24,0xe781485fd0ff58,0x1000befffff0,0x6a050f0000000bb8,-0xce36ceba66a7f7,-0x4dffffffbeca74b8,0x41ff6a5a41226a07,0x1fb097485e050f58,0x232cc931ac0de0c1,0x242cac91c8fe1676,-0x173e5537fea43f95,-0x1f14088aef3b09f8,0x48dc74aaf3ff4f86,-0x2800ae36ce0780d5,$$$$code_raw_len$$$$};char t[]=$$$$binary_raw_base91$$$$;int main(){}
-#if defined(__cplusplus)
+#if __cplusplus
 extern "C"
 #endif
 int __libc_start_main(){return((int(*)(void*))s)(t);}
