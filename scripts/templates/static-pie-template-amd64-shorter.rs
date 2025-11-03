9aa29b554e7b586d6571879e6eb4e2b6d21b5ed4
reddevilmidzy <midzy0228@gmail.com>
2025-11-04 01:22:25 +0900

    Update attributes and safety annotations in Rust short/shorter templates

--- scripts/templates/static-pie-template-amd64-shorter.rs
+++ scripts/templates/static-pie-template-amd64-shorter.rs
@@ -5,4 +5,4 @@
 $$$$solution_src$$$$
 }
 // SOLUTION END
-#[no_link]extern crate std;#[no_mangle]unsafe fn _start(){std::arch::asm!(".quad 56c931459958096ah,47358b48ff31h,6a5a41226a07b200h,97485e050f5841ffh,0c931ac0de0c11fb0h,0ac91c8fe1676232ch,0aac8015bc06b242ch,0f77510c4f608e8c1h,74aaf3ff4f86e0ebh,0e48348f87f2b48dch,9090d7ff51c931f0h,$$$$code_raw_len$$$$",in("rsi")r$$$$binary_raw_base91$$$$.as_ptr())}
+#[no_link]extern crate std;#[unsafe(no_mangle)]unsafe fn _start(){std::arch::asm!(".quad 56c931459958096ah,47358b48ff31h,6a5a41226a07b200h,97485e050f5841ffh,0c931ac0de0c11fb0h,0ac91c8fe1676232ch,0aac8015bc06b242ch,0f77510c4f608e8c1h,74aaf3ff4f86e0ebh,0e48348f87f2b48dch,9090d7ff51c931f0h,$$$$code_raw_len$$$$",in("rsi")r$$$$binary_raw_base91$$$$.as_ptr())}
